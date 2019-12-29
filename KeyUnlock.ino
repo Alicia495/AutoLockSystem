a55cd9f0262352f7ed4710f935dc99dbea52405a
rin-chan <a58040rm@kurume.kosen-ac.jp>
2019-12-29 20:07:25 +0900

    KeyAutoLock Ver1.1 miyasuku simasita

--- KeyUnlock.ino
+++ KeyUnlock.ino
@@ -11,39 +11,42 @@ void setup() {
   pinMode(A1,INPUT);//A
   pinMode(A0,INPUT);//B
 
-  pinMode(9,OUTPUT);//R
-  pinMode(10,OUTPUT);//G
-  pinMode(11,OUTPUT);//B
+  pinMode(9,OUTPUT);//LEDのR
+  pinMode(10,OUTPUT);//LEDのG
+  pinMode(11,OUTPUT);//LEDのB
   pinMode(8,OUTPUT);//SD
 
-  MsTimer2::set(200, corlor);
+  MsTimer2::set(200, corlor);//200msごとに色を変える（解錠時）
 }
 
-int r = 255,g = 0,b = 0;
 
 //0 130 65
-bool flag = true;
+bool flag = true;//鍵の状態を示すフラグ
+
 void loop() {
   // put your main code here, to run repeatedly:
-  int A = digitalRead(A1),B = digitalRead(A0);
-  digitalWrite(8,LOW);
-  //rainbow();
+  int A = digitalRead(A1),B = digitalRead(A0); //A1のみがlowになると解錠、A0のみがlowになると施錠します
+  digitalWrite(8,LOW);//サーボ電源用の降圧レギュレータ作動用のピン
   if(A != B){
     if(A == 0) {
       digitalWrite(9,HIGH);
       digitalWrite(10,HIGH);
       digitalWrite(11,LOW);
+      
       digitalWrite(8,HIGH);
       key.write(0);
-      //Serial.println(1000);
+      
       delay(2000);
+      
       key.write(65);
       delay(1000);
+      
       digitalWrite(9,LOW);
       digitalWrite(10,HIGH);
       digitalWrite(11,LOW);
+      
       delay(1000);
-      flag = 1;
+      flag = true;
       MsTimer2::start();
     }
     else if(B == 0) {
@@ -51,30 +54,34 @@ void loop() {
       digitalWrite(9,HIGH);
       digitalWrite(10,HIGH);
       digitalWrite(11,LOW);
+      
       digitalWrite(8,HIGH);
       key.write(120);
-      //Serial.println(-1000);
+      
       delay(2000);
+      
       key.write(65);
+      
       delay(1000);
+      
       digitalWrite(9,HIGH);
       digitalWrite(10,LOW);
       digitalWrite(11,LOW);
+      
       delay(1000);
+      
       digitalWrite(9,LOW);
       digitalWrite(10,LOW);
       digitalWrite(11,LOW);
-      flag = 0;
+      
+      flag = false;
     }
   }
-  Serial.print(A);
-  Serial.println("  ");
-  Serial.println(B);
   delay(5);
 }
 
 int sel = 0;
-void corlor(){
+void corlor(){　//7色で光らせる用の関数　順番に立ち上がる
   if(sel ==0){
     digitalWrite(9,HIGH);
     digitalWrite(10,LOW);
@@ -114,7 +121,9 @@ void corlor(){
   if(sel > 6) sel = 0;
 }
 
-void rainbow(){
+int r = 255,g = 0,b = 0;//なめらかな虹色を作るための変数
+
+void rainbow(){　//なめらかな虹色になる値を吐く関数　今は使用してない
   if(r >=255 && b <= 0 && g < 255) g++;
   else if(g >= 255 && b <= 0 && r > 0) r--;
   else if(g >= 255 && r <= 0 && b < 255) b++;
